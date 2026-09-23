4cabce611abc5a11f543bce14293ce10831d374b
Kevin Padilla <kspadilla@csi.edu>
2026-09-23 11:09:29 -0600

    lab in class

---
+++ WenesdayMorningClass-main/Week 6/Lab/notes/scripts.js
@@ -0,0 +1,70 @@
+//Comparison operators
+//=== equal to
+// !== not equal
+
+
+// console.log(1===1);
+// console.log(1!==1);
+
+
+// let score=85;
+// if (score >89){
+//     console.log("You got an A");
+// }
+
+// else if (score>79){
+//     console.log("You have a B");
+// }
+
+// else if (score>69){
+//     console.log("You got an C");
+// }
+
+// else{
+//     console.log("you have a mediorce grade");
+// }
+
+
+
+// let age=25;
+// let ismember=true;
+// if (age >=18){
+//     if(ismember){
+//         console.log("Adult member benefits applied");
+//     }
+//     else{
+//         console.log("Adult, but not a membership");
+//     }
+// } else {
+//     console.log("Minor")
+// }
+
+
+
+
+//we use Prompt() to make our websites more interactive
+// let score=85;
+// this variable is hardcoded, user cant change this
+// let score=Number(prompt("Enter your score (0-100"));
+// if (score >89){
+//     console.log("You got an A");
+// }
+
+// else if (score>79){
+//     console.log("You have a B");
+// }
+
+// else if (score>69){
+//     console.log("You got an C");
+// }
+
+// else{
+//     console.log("you have a mediorce grade");
+// }
+
+
+// document.body.innerHTML += "<p>The score you have is a  " + score + "</p>";
+
+// I want the website to ask the user for their name. and then display "hello [name]"
+// let username=prompt("what is your name? ")
+// document.body.innerHTML += "<p>Hello,  " + username + "</p>"
---
+++ WenesdayMorningClass-main/Week 6/Lab/scripts.js
@@ -0,0 +1,39 @@
+
+
+//Grade calculator
+let score=Number(prompt("Enter your score (0-100"));
+if (score >89){
+    console.log("You got an A");
+}
+
+else if (score>79){
+    console.log("You have a B");
+}
+
+else if (score>69){
+    console.log("You got a C");
+}
+else if (score>59){
+    console.log("You got a D");
+}
+
+else{
+    console.log("you got a F");
+}
+// // age check
+let age=Number(prompt("Enter your age"));
+if (age>=18){
+    console.log("You are a adult");
+}
+else{
+    console.log("You are a minor!");
+}
+
+//meber discount
+let ismember=(prompt("are you a member? (yes or no)"));
+if (ismember==="yes"){
+    console.log("Congrats you have a member discount");
+}
+else{
+    console.log("sorry you do not have a member discount");
+}
